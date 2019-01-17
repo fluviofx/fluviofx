d553396a8614e6972379f8b992380ac1773efbca
Lily Montoute <lily@thinksquirrel.com>
2019-01-17 17:16:06 -0600

    Implement Density Over Range operator

---
+++ Editor/Operators/DensityOverRange.cs
@@ -0,0 +1,49 @@
+using System.Linq;
+using Thinksquirrel.FluvioFX.Editor;
+using UnityEditor.VFX;
+using UnityEngine;
+
+namespace Thinksquirrel.FluvioFX.Editor.Operators
+{
+    [VFXInfo(category = "FluvioFX")]
+    class DensityOverRange : VFXOperator
+    {
+#pragma warning disable 649
+        public class InputProperties
+        {
+            public Vector2 Range = new Vector2(499.125f, 1247.8125f);
+        }
+        public class OutputProperties
+        {
+            public float t = 0;
+        }
+#pragma warning restore 649
+
+        public override string name
+        {
+            get
+            {
+                return "Density Over Range [0..1]";
+            }
+        }
+        protected override VFXExpression[] BuildExpression(VFXExpression[] inputExpression)
+        {
+            var density = VFXOperatorUtility
+                .ExtractComponents(new VFXAttributeExpression(FluvioFXAttribute.DensityPressure))
+                .ToArray() [1];
+
+            var range = VFXOperatorUtility.ExtractComponents(inputExpression[0]).ToArray();
+
+            return new VFXExpression[]
+            {
+                InverseLerp(density, range[0], range[1])
+            };
+        }
+
+        // TODO: Might be implemented by VFX graph in the future
+        private static VFXExpression InverseLerp(VFXExpression x, VFXExpression a, VFXExpression b)
+        {
+            return (x - a) / (b - a);
+        }
+    }
+}
