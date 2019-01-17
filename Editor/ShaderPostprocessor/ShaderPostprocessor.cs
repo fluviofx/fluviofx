a442c485b5d017442a94ef84dd135a0ba8e8f442
Lily Montoute <lily@thinksquirrel.com>
2019-01-17 04:33:20 -0600

    Fix failure cases with shader postprocessor

--- Editor/ShaderPostprocessor/ShaderPostprocessor.cs
+++ Editor/ShaderPostprocessor/ShaderPostprocessor.cs
@@ -6,6 +6,7 @@ using UnityObject = UnityEngine.Object;
 using System;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Thinksquirrel.FluvioFX.Editor;
 using Thinksquirrel.FluvioFX.Editor.Blocks;
 using UnityEngine.Experimental.VFX;
@@ -14,39 +15,40 @@ namespace Thinksquirrel.FluvioFX.Editor
 {
     internal static class ShaderPostprocessor
     {
-        // private const string solverData = "Texture2D Tex";
-        // private const string rwSolverData = "globallycoherent RWTexture2D<uint> Tex";
-        // private const string samplerState = "SamplerState samplerFluvioSolverData_a;";
-        // private const string sampler = "VFXSampler2D Tex";
-        // private const string rwSampler = "RWTexture2D<uint> Tex";
-        // private const string getSampler = "GetVFXSampler(FluvioSolverData_a, samplerFluvioSolverData_a)";
-        // private const string rwGetSampler = "FluvioSolverData_a";
-        private const string arg1 = "(float3 position,";
-        private const string arg2 = "(inout float3 position,";
-        private const string call1 = "(position,";
-        private const string call2 = "( /*inout */position,";
-        private const string indexArg1 = "(uint index, float3 position,";
-        private const string indexArg2 = "(uint index, inout float3 position,";
-        private const string indexCall1 = "(index, /*inout */position,";
-        private const string indexCall2 = "(index, position,";
+        private static string[] patternFormat = new []
+        {
+            "void {0}(_[0-9A-F]+){{0,1}}\\((?!uint index)",
+            "(?<!void ){0}(_[0-9A-F]+){{0,1}}\\((?!index)",
+        };
+        private static string[] replacementFormat = new []
+        {
+            "void {0}$1(uint index, ",
+            "{0}$1(index, ",
+        };
 
         public static StringBuilder ModifyShader(VFXContext context, StringBuilder source)
         {
+            var blockFunctionNames = typeof(FluvioFXBlock)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(FluvioFXBlock)) && !t.IsAbstract)
+                .Select(t => t.Name);
+
             if (context.activeChildrenWithImplicit.Any((b) => typeof(FluvioFXBlock).IsAssignableFrom(b.GetType())))
             {
-                source
-                    // .Replace(rwSolverData, solverData)
-                    // .Replace(rwSampler, sampler)
-                    // .Replace(solverData, rwSolverData)
-                    // .Replace(samplerState, "")
-                    // .Replace(sampler, rwSampler)
-                    // .Replace(getSampler, rwGetSampler)
-                    .Replace(arg1, indexArg1)
-                    .Replace(arg2, indexArg2)
-                    .Replace(call1, indexCall1)
-                    .Replace(call2, indexCall2);
-            }
+                var result = source.ToString();
+                foreach (var functionName in blockFunctionNames)
+                {
+                    for (var i = 0; i < patternFormat.Length; ++i)
+                    {
+                        var pattern = string.Format(patternFormat[i], functionName);
+                        var replacement = string.Format(replacementFormat[i], functionName);
+                        result = Regex.Replace(result, pattern, replacement);
+                    }
+                }
 
+                return new StringBuilder(result);
+            }
             return source;
         }
     }
